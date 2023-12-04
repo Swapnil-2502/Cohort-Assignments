db0c57b3df08010842edd3884a39bed7f93cb676
Swapnil-2502 <hajareswapnil.2502@gmail.com>
2023-12-04 16:31:19 +0530

    countVowels done

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -6,7 +6,19 @@
 */
 
 function countVowels(str) {
-    // Your code here
+    let count = 0;
+
+    let newStr = str.toLowerCase()
+
+    for(let i=0;i<newStr.length;i++){
+      if(newStr[i]=='a' ||
+      newStr[i]=='e' ||
+      newStr[i]=='i' ||
+      newStr[i]=='o' ||
+      newStr[i]=='u') count++;
+    }
+
+    return count;
 }
 
 module.exports = countVowels;
