96c6a037e5985bfa2857d4ce073f527ee3f760c9
Swapnil-2502 <hajareswapnil.2502@gmail.com>
2023-12-04 16:25:21 +0530

    expenditure-analysis done

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -6,7 +6,26 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+
+  const TotalSpendByCategory = {};
+
+  transactions.forEach(transaction =>{
+    const {category, price} = transaction;
+
+    if(!TotalSpendByCategory[category]){
+      TotalSpendByCategory[category] = price
+    }
+    else{
+      TotalSpendByCategory[category] += price
+    }
+  })
+
+  const result = Object.keys(TotalSpendByCategory).map(category => ({
+    category,
+    totalSpent: TotalSpendByCategory[category]
+  }));
+
+  return result;
 }
 
 module.exports = calculateTotalSpentByCategory;
