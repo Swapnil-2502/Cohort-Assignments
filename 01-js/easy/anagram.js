147c0a8419f4cbfbc23fafc5098baa2403e7b16c
Swapnil-2502 <hajareswapnil.2502@gmail.com>
2023-12-04 16:05:34 +0530

    easy anagram completed

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -6,6 +6,35 @@
 
 function isAnagram(str1, str2) {
 
+  
+
+  const cleanStr1 = str1.replace(/\s/g, '').toLowerCase();
+  const cleanStr2 = str2.replace(/\s/g, '').toLowerCase();
+
+  if(cleanStr1.length != cleanStr2.length) return false;
+
+  const frequency1 = {};
+  const frequency2 = {};
+
+  // Count the frequency of characters in the second string
+  for (let char of cleanStr1) {
+    frequency1[char] = (frequency1[char] || 0) + 1;
+  }
+
+  // Count the frequency of characters in the second string
+  for (let char of cleanStr2) {
+    frequency2[char] = (frequency2[char] || 0) + 1;
+  }
+
+  // Compare the character frequencies
+  for (let char in frequency1) {
+    if (frequency1[char] !== frequency2[char]) {
+      return false;
+    }
+  }
+
+  return true;
+
 }
 
 module.exports = isAnagram;
