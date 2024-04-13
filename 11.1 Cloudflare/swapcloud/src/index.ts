931477a304ec7196013a48635fd2f6d2623999a2
Swapnil-2502 <hajareswapnil.2502@gmail.com>
2024-04-13 16:50:22 +0530

    11.1 Cloudflare added

---
+++ 11.1 Cloudflare/swapcloud/src/index.ts
@@ -0,0 +1,25 @@
+
+export interface Env {
+	
+}
+
+export default {
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		
+		if (request.method === "GET") {
+			return Response.json({
+				message: "you sent a get request"
+			});
+		} else {
+			return Response.json({
+				message: "you did not send a get request"
+			});
+		}
+	},
+};
+
+// export default {
+// 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+// 		return new Response('Hello World!');
+// 	},
+// };
